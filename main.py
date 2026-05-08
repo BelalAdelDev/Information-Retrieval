be3390f2ecfe7553073e77a8e1a2624744b2f658
Belal Adel <belaladel8032@gmail.com>
2026-05-08 20:46:28 +0300

    Added Examples  - Precision  - Recall

--- main.py
+++ main.py
@@ -30,8 +30,51 @@ def run_app(engine: SearchEngine) -> None:
 
 
 def main() -> None:
+    test()
     engine = SearchEngine()
     run_app(engine)
+
+def test() -> None:
+    engine = SearchEngine()
+    ground_truth = {
+        "artificial intelligence": ["Artificial_intelligence.txt", "Deep_learning.txt"],
+        "computer memory": ["Random-access_memory.txt", "Computer_hardware.txt"],
+        "نظام التشغيل": ["نظام_التشغيل_لينكس.txt", "نظام_التشغيل.txt"],
+        "هتلر": ["الحرب_العالمية_الثانية.txt"], # <----- اهم حاجة دا 
+        "كمبيوتر": ["امن_الحاسوب.txt", "وحدة_معالجة_مركزية.text", "جدار_حماية_(حوسبة).txt"]
+    }
+    
+    for query, relevant_docs in ground_truth.items():
+        print(f"Testing Query: '{query}'")
+        
+        results = engine.search(query)
+        retrieved_docs = []
+        
+        for docId, _ in results:
+            doc_name = engine.docIdMap.get(docId, "Unknown")
+            retrieved_docs.append(doc_name)
+            
+        print(f"Retrieved {len(retrieved_docs)} docs: {retrieved_docs}")
+        print(f"Relevant Expected: {relevant_docs}")
+        
+        retrieved_set = set(retrieved_docs)
+        relevant_set = set(relevant_docs)
+        
+        true_positive = len(retrieved_set.intersection(relevant_set))
+        
+        if len(retrieved_set) > 0:
+            precision = true_positive / len(retrieved_set)
+        else:
+            precision = 0.0
+        
+        if len(relevant_set) > 0: 
+            recall = true_positive / len(relevant_set)
+        else:
+            recall = 0.0
+    
+        print(f"Precision: {precision:.2f}")
+        print(f"Recall: {recall:.2f}")
+        print("====================================")
 
 if __name__ == "__main__":
     main()
